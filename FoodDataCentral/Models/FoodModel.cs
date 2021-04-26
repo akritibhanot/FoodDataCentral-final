0082a9262b919ad0f343b1e46e058422e84f8cb3
Akriti Bhanot <akritibhanot@usf.edu>
2021-04-25 23:30:30 -0400

    Changed the View Model class

--- FoodDataCentral/Models/FoodModel.cs
+++ FoodDataCentral/Models/FoodModel.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace FoodDataCentral.Models
 {
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    // child class declaration -
     public class FoodNutrient
     {
         [Key]
